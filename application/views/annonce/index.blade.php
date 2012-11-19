2a851f37206e2875f1b016d1ee5cbfd7fc36989b
xavier Bekaert <xavier24@hotmail.com>
2012-11-19 15:39:51 +0100

    creation de lien url

--- application/views/annonce/index.blade.php
+++ application/views/annonce/index.blade.php
@@ -5,7 +5,8 @@
             <h2>Départ : {{$annonce->depart}}</h2>
             <h2>Arrivée : {{$annonce->arrivee}}</h2>
             <p>le {{$annonce->date}}</p>
-            <p>Par {{$annonce->user->username}}</p>
+            <p><a href="{{URL::base().'/annonce/lire/'.Str::slug($annonce->depart.'-'.$annonce->arrivee).'/'.$annonce->id}}">Voir l'annonce</a></p>
+            <p>Par <a href="{{URL::base().'/user/voir/'.Str::slug($annonce->user->username).'/'.$annonce->user->id}}">{{$annonce->user->username}}</a></p>
             <hr />
         @endforeach
     @endif
