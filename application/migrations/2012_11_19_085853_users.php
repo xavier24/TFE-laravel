4409faac3831ba7c22874371c5cc4b9dfe2a4e4c
xavier Bekaert <xavier24@hotmail.com>
2012-11-19 10:03:35 +0100

    creation table "users"

---
+++ application/migrations/2012_11_19_085853_users.php
@@ -0,0 +1,37 @@
+<?php
+
+class Users {
+
+	/**
+	 * Make changes to the database.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+            Schema::create('users',function($table){
+               $table->increments('user_id');
+               $table->string('username',20);
+               $table->string('email');
+               $table->string('password');
+               $table->boolean('conducteur');
+               $table->boolean('fumeur');
+               $table->boolean('bagage');
+               $table->boolean('animaux');
+               $table->boolean('discussion');
+               $table->boolean('musique');
+               $table->timestamps();
+            });
+	}
+
+	/**
+	 * Revert the changes to the database.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+            Schema::drop('users');
+	}
+
+}
